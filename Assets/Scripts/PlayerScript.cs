a08f84bb2addab44c6ded54e031316a6992cc41c
ANDRE VICTOR STEPHANES VALVASSORI <andre.valvassori@pucpr.edu.br>
2019-05-18 16:42:27 -0300

    Aula 1805

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    // public variables
+    public float BaseVelocity;
+    public float BaseMultiplier;
+    public int tickUpdate = 60;
+    public int MaxPericia = 20;
+
+    // public Stats variables
+    public int CurrentPericia;
+
+    // public components!
+    public Rigidbody2D Player;
+    public Button AttackButton;
+    public Text ButtonText;
+    public Text PericiaText;
+
+
+    // Make this private!
+    public float CurrentVelocity;
+    public int currentTick = 0;
+    //
+    public string direcao = "direita";
+    void Start()
+    {
+        Player = gameObject.GetComponent < Rigidbody2D >() as Rigidbody2D;
+        CurrentVelocity = BaseVelocity;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        currentTick += 1;
+
+        // Check Player Touch
+        if (Input.GetKey("d"))
+        {
+            direcao = "direita";
+        } else
+        if (Input.GetKey("a"))
+        {
+            direcao = "esquerda";
+        }
+
+        // Update Player Velocity.
+        if (direcao == "direita")
+        {
+            Player.velocity = new Vector2(CurrentVelocity, Player.velocity.y);
+        }
+        else
+        {
+            Player.velocity = new Vector2(-CurrentVelocity, Player.velocity.y);
+        }
+
+        //
+        if (CurrentPericia >= MaxPericia) AttackButton.interactable = true; else AttackButton.interactable = false;
+
+        // Exec TickUpdate Routine.
+        if (currentTick >= tickUpdate)
+        {
+            CurrentVelocity = CurrentVelocity + (CurrentVelocity * BaseMultiplier);
+
+            if (CurrentPericia < MaxPericia) CurrentPericia += 1;
+
+            PericiaText.text = ($"Pericia: " +  CurrentPericia.ToString());            
+
+            currentTick = 0;
+        }
+    }
+
+    public void ButtonAttackClick()
+    {
+        CurrentPericia = 0;
+    }
+    public void ButtonMoveClick()
+    {
+        if (direcao == "esquerda")
+            direcao = "direita";
+        else
+            direcao = "esquerda";            
+    }
+
+}
---
+++ Assets/Scripts/Wizard.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wizard : MonoBehaviour
+{
+    public string CurrentAction;
+
+    private Vector3 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CurrentAction == "")
+        {
+            switch (Random.Range(1, 4))
+            {
+                case 1:
+                    CurrentAction = "MoveLeft";
+                    Move("Left");
+                    break;
+                case 2:
+                    CurrentAction = "MoveRight";
+                    Move("Right");
+                    break;
+                case 3:
+                    CurrentAction = "Fire";
+                    Fire(0);
+                    break;
+                //case 4:
+                //    CurrentAction = "DoNothing";
+                //    break;
+            };            
+        }
+
+
+    }
+
+    public void Move(string type)
+    {
+        if(type == "Left")
+        {
+            if(transform.position.x <= -2.0f)
+            {
+                Move("Right");
+                CurrentAction = "";
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+                CurrentAction = "";
+            }
+        }
+        else
+        {
+            if (transform.position.x >= 2.0f)
+            {
+                Move("Left");
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+                CurrentAction = "";
+            }
+
+        }
+    }
+
+    public void Fire(int Random)
+    {
+        CurrentAction = "";
+    }
+
+}
